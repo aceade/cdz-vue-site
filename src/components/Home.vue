1876e32b6a80e354962874c690a96210e89cd5f9
aceade@github.com <philiprowlands90@gmail.com>
2022-07-05 11:07:24 +0100

    #12 cleaning up text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,7 @@
     <p>Until the 1850s.</p>
 
     <h2>The Disaster</h2>
-    <p>Some say that the dwarves had developed an electrical power source that involved splitting particles of air. Others that it was divine retribution for unspeakable acts.</p>
+    <p>Some say that it was caused by an attempt to generate electricity by splitting particles of air. Others that it was divine retribution for unspeakable acts.</p>
     <p>Nobody knows for certain what really happened, but what everyone knows is that the very fabric of reality was torn asunder. 
         What was once a series of impregnable cities became a nightmare where gravity could suddenly reverse, where the unfortunate could take another step and burst into flame, where one could turn a corner and vanish into thin air, only to reappear months later with no warning.
         To say nothing of the monsters that appeared.
