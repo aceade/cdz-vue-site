4b3bda3082941fdf2e9c5e3c57a882277c13c4bf
aceade@github.com <philiprowlands90@gmail.com>
2022-07-21 18:03:56 +0100

    #28 adding slideshow component

--- src/Evictor.vue
+++ src/Evictor.vue
@@ -5,60 +5,60 @@
     <h2>Smash The Evictor's Statue!</h2>
     <p class="welcome">In which a pair of serpentoid mercenaries are less than impressed with one of their siblings</p>
 
-    <splide id="splide_comic" aria-labelledby="scenery" :options="options" :has-track="false">
-
-        <div class="splide__progress">
-            <div class="splide__progress__bar">
-            </div>
-        </div>
-
-        <splide-track>
-            <SplideSlide v-for="slide in slides" :key="slide.alt">
-              <img :src="slide.src" :alt="slide.alt" />
-          </SplideSlide>
-        </splide-track>
-    </splide>
+    <Slideshow :options="slide_options" :slides="slides" :has-track="false"></Slideshow>
   </main>
 </template>
 
-<script setup>
+<script>
 import Navbar from './components/Navbar.vue';
+import Slideshow from './components/Slideshow.vue';
 
-import { Splide, SplideTrack, SplideSlide } from '@splidejs/vue-splide';
-
-const options = {
-  rewind  : true,     // go back to the start when done
-  gap     : '1rem',   // gap between images
-  autoplay: true,
-  mediaQuery: 'min',
-  autoWidth: false,
-  autoHeight: true,
-};
-const slides = [{
-    src: "https://aceade.files.wordpress.com/2022/03/panel1-1.jpg",
-    alt: "A humanoid snake trying to look adorable or innocent. A speech bubble says 'Let me get this straight, Sectaa...'",
+export default {
+  components: {
+    Navbar,
+    Slideshow
   },
-  {
-    src: "https://aceade.files.wordpress.com/2022/03/panel2.jpg",
-    alt: "Two humanoid snakes staring out of the page. One has their arms folded and is saying 'After hearing one line from the Fomorian shaman'. The other is facepalming",
-  },
-  {
-    src: "https://aceade.files.wordpress.com/2022/03/panel3.jpg",
-    alt: "A monk or priest in the middle of a sermon. He is saying 'Like St Patrick drove the snakes before him', before catching himself",
-  },
-  {
-    src: "https://aceade.files.wordpress.com/2022/03/panel4-1.jpg",
-    alt: "A humanoid snake trying to enter a building. They hold a crowbar in their left claw. From offscreen, somebody is saying 'You decided to sneak into the local temple'",
-  },
-  {
-    src: "https://aceade.files.wordpress.com/2022/03/panel5-1.jpg",
-    alt: "A statue in the middle of a shaft of light. In the foreground, a humanoid snake's left claw looms out of the shadows, clutching a crowbar. From offscreen, somebody is saying 'And try to smash a statue of a shaman who died centuries ago'",
-  },
-  {
-    src: "https://aceade.files.wordpress.com/2022/03/panel6.jpg",
-    alt: "Two humanoid snakes staring out of the page. One has their arms folded and is saying 'In the slide-up to this \"St Patrick's Day\"'. The other is facepalming and asks \"How much tae have you been drinking?\" ",
+  data() {
+    return {
+      el_id: "splide_comic",
+      slide_options : {
+        rewind  : true,     // go back to the start when done
+        gap     : '1rem',   // gap between images
+        autoplay: true,
+        mediaQuery: 'min',
+        autoWidth: false,
+        autoHeight: true,
+      },
+      slides: [{
+        src: "https://aceade.files.wordpress.com/2022/03/panel1-1.jpg",
+        alt: "A humanoid snake trying to look adorable or innocent. A speech bubble says 'Let me get this straight, Sectaa...'",
+      },
+      {
+        src: "https://aceade.files.wordpress.com/2022/03/panel2.jpg",
+        alt: "Two humanoid snakes staring out of the page. One has their arms folded and is saying 'After hearing one line from the Fomorian shaman'. The other is facepalming",
+      },
+      {
+        src: "https://aceade.files.wordpress.com/2022/03/panel3.jpg",
+        alt: "A monk or priest in the middle of a sermon. He is saying 'Like St Patrick drove the snakes before him', before catching himself",
+      },
+      {
+        src: "https://aceade.files.wordpress.com/2022/03/panel4-1.jpg",
+        alt: "A humanoid snake trying to enter a building. They hold a crowbar in their left claw. From offscreen, somebody is saying 'You decided to sneak into the local temple'",
+      },
+      {
+        src: "https://aceade.files.wordpress.com/2022/03/panel5-1.jpg",
+        alt: "A statue in the middle of a shaft of light. In the foreground, a humanoid snake's left claw looms out of the shadows, clutching a crowbar. From offscreen, somebody is saying 'And try to smash a statue of a shaman who died centuries ago'",
+      },
+      {
+        src: "https://aceade.files.wordpress.com/2022/03/panel6.jpg",
+        alt: "Two humanoid snakes staring out of the page. One has their arms folded and is saying 'In the slide-up to this \"St Patrick's Day\"'. The other is facepalming and asks \"How much tae have you been drinking?\" ",
+      }],
+      hasTrack: false
+    }
   }
-];
+}
+
+
 </script>
 
 <style>
---
+++ src/components/Slideshow.vue
@@ -0,0 +1,62 @@
+<template>
+    
+    <splide :id="el_id" :options="options" :has-track="has-track">
+
+        <div class="splide__progress">
+            <div class="splide__progress__bar">
+            </div>
+        </div>
+
+        <splide-track>
+            <SplideSlide v-for="slide in slides" :key="slide.alt">
+              <img :src="slide.src" :alt="slide.alt" />
+          </SplideSlide>
+        </splide-track>
+    </splide>
+
+</template>
+
+<script>
+import { Splide, SplideTrack, SplideSlide } from '@splidejs/vue-splide';
+
+export default {
+    props: {
+        slides: Array,
+        options: Object,
+        hasTrack: Boolean,
+        el_id: String
+    },
+    components: {
+        Splide, SplideTrack, SplideSlide
+    }
+}
+</script>
+
+<style>
+@import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
+@media (min-width: 1024px) {
+  .splide img {
+    max-width: 800px;
+  }
+}
+@media (min-width: 800px) and (max-width: 900px) {
+  .splide img {
+    max-width: 750px;
+  }
+}
+@media (max-width: 800px) and (min-width: 700px) {
+  .splide img {
+    width: 500px
+  }
+}
+@media (max-width: 500px) {
+  .splide img {
+    max-width: 300px;
+  }
+}
+
+#splide01 {
+    max-width: 900px;
+}
+</style>
