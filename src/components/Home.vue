52bbff829827ed6baf1e4a1dc9b1b2d18e841c17
aceade@github.com <philiprowlands90@gmail.com>
2022-07-31 14:11:01 +0100

    #45 adding sketches

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,15 +14,55 @@
         What was once a series of impregnable cities became a nightmare where gravity could suddenly reverse, where the unfortunate could take another step and burst into flame, where one could turn a corner and vanish into thin air, only to reappear months later with no warning.
         To say nothing of the monsters that appeared.
     </p>
-    <ul>
-        <li>The banshees - tall, thin humanoids with screeching voices and whose dress and behaviour resembled nothing short of the Vikings.</li>
-        <li>The serpentoids - humanoid snakes seeking their revenge on Saint Patrick.</li>
-        <li>The púca. Mutated horses with the ability to mimic human speech.</li>
-        <li>And the Fomorians. The 'orcs'. The mutated remains of the humans and dwarves who had called the region home and were left behind.</li>
-    </ul>
+        <Slideshow :options="options" :slides="slides" :collapsible="collapsible"/>
 
     <p class="welcome">Welcome to the West Connacht Disaster Zone.</p>
 </template>
+
+<script>
+import Slideshow from './Slideshow.vue';
+
+export default {
+  components: {
+    Slideshow,
+  },
+  data() {
+    return {
+        collapsible: false,
+        slides: [{
+            src: "/images/Monsters_Banshee.png",
+            alt: "A thin humanoid dressed in a cloak and tunic, clutching a sword",
+            caption: "The banshees - tall, thin humanoids with screeching voices and whose dress and behaviour resembled nothing short of the Vikings."
+        },
+        {
+            src: "/images/Monsters_Snake.png",
+            alt: "A humanoid snake exposing their fangs",
+            caption: "The serpentoids - humanoid snakes seeking their revenge on Saint Patrick."
+        },
+        {
+            src: "/images/Monsters_Puca.png",
+            alt: "A red-eyed horse staring at the camera with a distended smirk on it's face",
+            caption: "The púca. Mutated horses with the ability to mimic human speech and an unlimited capacity for mischief."
+        },
+        {
+            src: "/images/Monsters_orc.png",
+            alt: "A tusked human with red hair and green eyes",
+            caption: "And the Fomorians. The 'orcs'. The mutated remains of the humans and dwarves who had called the region home and were left behind."
+        }],
+        options: {
+            rewind  : true,
+            gap     : '5rem',
+            autoplay: true,
+            autoWidth: false,
+            autoHeight: false,
+            mediaQuery: 'min',
+        }
+    }
+
+  }
+}
+
+</script>
 
 <style scoped>
 
