3bb069c6c9adde22104a6b80f0f20bf7d67579c7
aceade@github.com <philiprowlands90@gmail.com>
2022-11-26 18:49:41 +0000

    #79 fixing bug with PDF slideshow

--- src/components/stories/DetentionCentreTales.vue
+++ src/components/stories/DetentionCentreTales.vue
@@ -11,12 +11,11 @@ export default {
     },
     data() {
         return {
-            // each story has a PDF link, title and fallback text/intro
+            // each story has a PDF link, title and fallback text/paragraphs
             stories: [
                 {
-                    src: "",
                     title: "Maladaptive Vigilance",
-                    intro: [
+                    paragraphs: [
                         "Maebh stared at the mirror, her eyes following her fingers up along the stripes on her new blouse’s v-shaped front collar. She smiled, revealing more teeth that had once been much more yellow. Finally, she had something to wear that wasn’t bleached or grey nettle-fibres issued from the laundry – something that was hers. Lowering her hands, she turned towards the younger girl in the room to ask what she thought.",
                         "“Why does the collar hang down your back like that?” Aoife asked, swinging her legs idly as she sat on the table next to the sink. Her left hand briefly lifted to point at Maebh’s left shoulder, before returning to rest on her lap. Maebh shrugged, not having cared enough to ask why a sailor’s outfit had such a striped square collar, and nobody had thought it necessary to explain it.",
                         "“I want one too,” the other girl admitted enviously. She glanced down at her own grey jumper and off-white dress. Both were clean and the original tears had been well repaired, but they were so...so plain and impersonal. At least they didn’t clash with her ginger hair.",
@@ -24,9 +23,8 @@ export default {
                     ]
                 },
                 {
-                    src: "",
                     title: "Samples",
-                    intro: [
+                    paragraphs: [
                         "The knot in Diarmuid’s stomach was growing tighter. He’d been lying or sitting on the infirmary bed for nearly two hours – if the clock on the wall was right – ever since cleaning his teeth after breakfast. He did not want to be there, and the tongue lashing he’d received when saying so did not help.",
                         "He glanced around the room again. Bare white walls, alternating grey and white tiles on the floor, and seven more beds. Locked medicine cabinets and a couple of workbenches lined the far wall, with a sink halfway down the room. The unwanted smell of antiseptic came from a door at the far end of the room – and he jerked his head away, trying to avoid the word printed on the door. Belatedly, he noticed that there was a compact gramophone on a table down the other end.",
                         "“A-are you nervous t-too?”",
@@ -34,9 +32,8 @@ export default {
                     ]
                 },
                 {
-                    src: "",
                     title: "Rage Against The Mirror Self",
-                    intro: [
+                    paragraphs: [
                         "Siobhán lowered her hands into the stream of the tap, savouring the flash of warmth as the lukewarm water hit her soil-covered skin. After five hours of gardening in a draughty, half-leaking greenhouse, it was good to just be alone for five minutes. Unfortunately, it couldn’t last, as one of Maebh’s teasing comments about herself and Diarmuid came unbidden to the fore.",
                         "“Come on, Shiva. The doctors aren’t going to care if you hug him once.”",
                         "Shiva. Diarmuid’s nickname for her. A name he genuinely hadn’t known was that of the Hindu god of destruction. In fact, he hadn’t even known of them – he’d just “heard the name somewhere”. It was still a pagan god of destruction...",
@@ -46,9 +43,8 @@ export default {
                     ]
                 },
                 {
-                    src: "",
                     title: "The Dwarven Tea Machine",
-                    intro: [
+                    paragraphs: [
                         "The machine that stood on the counter gleamed. Its shiny metallic body rose from a rounded, well-polished wooden base, curving up towards a boxy tower on one end and an alcove at the other. An array of brass plates adorned the front of the machine, marking the buttons, rounded glass light housings and an embossed slot in a font that appeared to have been delicately carved from the metal itself. In one corner, a thick black cable snaked around behind towards the wall.",
                         "“Is that made of silver?” Aoife asked, pointing at the machine, eliciting a few smiles from the other children in the room as they gathered around it. Her twin brother Ciaran gingerly reached forward to tap at the corner near him, where somebody had intricately carved some larger words and the number 418 in what looked like English. His face and body immediately slumped as he found out that it felt too hard to be silver.",
                         "“What is it, anyway?”",
@@ -56,9 +52,8 @@ export default {
                     ]
                 },
                 {
-                    src: "",
                     title: "Washing Up Wrangling",
-                    intro: [
+                    paragraphs: [
                         "“Hey, Red!”",
                         "Diarmuid morosely looked up from the lukewarm sink that held two stained metal plates, and narrowed his eyes as he beheld the stocky younger boy with the fangs and green patches of skin next to the draining board. “What is it, Grady?”",
                         "“Me name’s Tomás. Nurse said you could use a hand.”",
@@ -75,7 +70,7 @@ export default {
                 perPage: 1,
                 rewind: true
             },
-            objectType: "pdf",
+            objectType: "story",
         }
     }
 }
